6acece88656eb30376638d0bbf02a5b5d385a7c7
szamano <marekdeja@wp.pl>
2018-01-04 15:26:07 +0100

    Add files via upload

---
+++ src/components/posts_new.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {createPost} from '../actions';
+
+class PostsNew extends Component {
+
+  renderField(field){
+    //const {meta}=field; //meta=field.meta
+    const {meta: {touched, error}}=field;
+    //const className = `form-group ${meta.touched&&meta.error?'has-danger':''}`;
+const className = `form-group ${touched&&error?'has-danger':''}`;
+    return(
+      <div className={className}>
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+        <div className="text-help">
+        {touched?error:''}
+      </div>
+      </div>
+    );
+  }
+
+onSubmit(values){
+//  console.log(values);  //this===component
+this.props.createPost(values, ()=>{
+  this.props.history.push('/');
+} );
+}
+
+  render() {
+const {handleSubmit}=this.props;
+
+    return (<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+      <Field
+        label="Title"
+        name="title"
+        component={this.renderField}
+      />
+      <Field
+        label="Categories"
+        name="categories"
+        component={this.renderField}/>
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}/>
+          <button type="submit" className="btn btn-primary">Submit</button>
+          <Link to="/" className="btn btn-danger">Cancel</Link>
+    </form>);
+
+  }
+}
+
+function validate(values){
+  //console.log(values)->{title: 'asfd', categories: 'gfsg', content :'sff'}
+const errors={};
+
+// Validate the inputs from 'values'
+if (!values.title||values.title.length<3){
+  errors.title="Enter a title with at least 3 characters!";
+}
+if (!values.categories){
+  errors.categories='Enter some categories';
+}
+if (!values.content){
+  errors.content='Enter some content';
+}
+//if errors is emtpy, the form is fine to submit
+//if errors has *any* properties, redux form assumes form
+return errors;
+}
+
+export default reduxForm({
+  validate,//validate: validate
+  form: 'PostsNewForm' //unique
+})(
+  connect(null, {createPost})(PostsNew)
+);
